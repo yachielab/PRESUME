08ec6c0c21402b28400517635e39d65c4cd8b75b
Keito Watano <42859818+k10watano@users.noreply.github.com>
2019-08-03 09:56:16 +0900

    Update PRESUME_help.py

--- PRESUME_help.py
+++ PRESUME_help.py
@@ -1,52 +1,64 @@
 helpdoc="""
-   shellv$ python3 PRESUME.py -h
-   usage: PRESUME.py [-h] [--load FileName] [-V] [--limit TimeLimit] [-n NumberOfSequences] 
-   		              [-L SequenceLength] [-f FASTAfile] [-d DoublingTime] 
-   		              [-s StandardDeviation] [-e DeletionProbability] 
-   		              [--model ModelParameter] [-u NumberOfSequences] [-m SubstitutionRate]
-   		              [--delta SubstitutionFrequency] [--qsub] [--debug] [--bar] [--save]
-                     [--no_retry]
-   
-   PRESUME.py
-    
-    optional arguments:
-    -h, --help           shows this help message and exit.
-    --load FILENAME			 loads argument from the specified file.
-    										 (The format is same as the csv file PRESUME outputs when --option attached.)
-    -V, --version        shows version of PRESUME.
-    --limit LIMIT        specifies the time limit (default=1)
-    -n N                 specifies required number of sequences: when you specified this
-                         option, the time limit becomes the time point when N                   
-                         sequences are present for the first time. (default=None)
-    -L L                 specifies the length of a sequence (default=1000)
-    -f F                 specifies the fasta file name　of the initial sequence.
-                         (default: The initial sequence will be randomly generated.)
-    -d D                 specifies the doubling time of the initial sequence (default=1)
-    -s S                 specifies the standard deviation of doubling time of the initial      
-                         sequence (default=0)
-    -e E                 specifies the random deletion probability: any sequence is
-                         randomly deleted at the probability of E when it 
-    										 emerges (default=0)
-    --output OUTPUT      specifies the output folder (default:current working directory)
-    --model MODEL        specifies parameters for substitution rate matrix 
-    										 Format： --model GTR{A-C/A-G/A-T/C-G/C-T/G-T}+FU{piA/piC/piG/piT}+G{shape of gamma distribution}
-    										 For details, please also refer another document on substitution model (<link>)
-                         Or, you can use default parameters by 
-                         --model default
-                         (default: GTR{GTR{0.03333/0.03333/0.03333/0.03333/0.03333/0.03333}
-                         +FU{0.25/0.25/0.25/0.25}+G4{10000} )
-    -u U                 specifies the upper limit of number of sequences for safety (default=2^20)
-    -m M                 specifies the mean of relative substitution rate following gamma distribution (default=1)
-    --delta DELTA        specifies the fixed substitution frequency of every site in time-independent model (default=None)
-    --qsub               activates the distributed computing mode (defalt=inactivated)
-    --debug              inactivates the deletion of intermediate files
-    --bar                activates showing progress bar (python tqdm module is required.)
-    --save               saves the argument information in args.csv file
-    --no_retry           inactivates retrying simulation when all sequences are deleted.
-    --seed							 spacifies the seed of random number
-    --polyC              set the initial sequence as poly-C.
-    -r R								 specifies maximum iteration number:
-                         If all sequences are deleted, PRESUME.py repeats the simulation from the beginning at most R times.
+Usage:
+    PRESUME.py 
+    [-v] [--version] [-h] [--help] [-n sequence_number] [-L sequence_length] [-s standard_deviation]
+    [-e extinction_probability] [--gtrgamma model_parameters] [-m mean_substitution_rate]
+    [--timeind substitution_probability] [--qsub] [--output directory_path] [-f input_file]
+    [--load file_name] [-u sequences_number] [--debug] [--bar] [--save] 
+    [-r max_retrial_number] [--seed random_seed] [--limit time_limit] 
+
+Options:
+    -v --version
+      Print PRESUME version; ignore all the other parameters
+    -h --help
+      Print the usage of PRESUME; ignore all the other parameters
+    -n <Integer>
+      Number of sequences to be generated. Default: 100
+    -L <Integer>
+      Length of sequences to be generated. Default: 1000
+    -s <Float>
+      Standard deviation of propagation speed. Default: 0
+    -e <Float>
+      Probability of extinction. Default: 0
+    --gtrgamma <String>
+      Execute GTR-Gamma model to simulate sequence diversification with its parameters
+        Parameter format： --gtrgamma GTR{A-C/A-G/A-T/C-G/C-T/G-T}+FU{piA/piC/piG/piT}+G{alpha}
+        For more details, see https://github.com/yachielab/PRESUME/blob/master/SubstitutionModelDetails.PRESUME.pdf
+        or you can use the default parameter set by 
+          --gtrgamma default
+        which is equivalent to
+          --gtrgamma GTR{0.03333/0.03333/0.03333/0.03333/0.03333/0.03333}+FU{0.25/0.25/0.25/0.25}+G4{10000}
+    -m <Float>
+        Mean of gamma distribution for relative substitution rates of different sequence 
+          Positions. Default: 1
+    --constant <Float>
+　　　　　　Execute time-independent model to simulate sequence diversification with a parameter of
+          constant substitution probability per generation of every sequence position
+    --qsub
+        Execute the distributed computing mode
+    --output <String>
+        Output directory path. PRESUME creates a directory unless exists. Default: current 
+          directory
+    -f <String>
+        Input FASTA file name　for the root sequence. Random sequence will be generated 
+          unless specified
+    --param <String>
+        Basic parameter values of PRESUME can be input as a csv file (the format of the file is same 
+          for which is output by PRESUME when -–save is specified)
+    -u <Integer>
+        Maximum number of sequences to be generated. Default: 1000000000
+    --debug
+        Output intermediate files
+    --bar
+        Activate the monitoring of simulation progress with Python tqdm module
+    --save
+        Output basic parameter values used
+    -r <Integer>
+        Maximum number of retrials of simulation when all sequences are extinct
+    --seed <Integer>
+        Seed number for generation of random values. Default: 0
+    --monitor <float>
+　　　　　　Monitoring paramater. Default: 1
 """
 
 def help_description():
