722378d207093ed34e00f370079f67d7845d57e2
UTNK <0ratnaramatn1n@gmail.com>
2020-06-21 15:52:18 +0900

    debug

--- PRESUME.py
+++ PRESUME.py
@@ -499,7 +499,7 @@ def survey_all_dead_lineages(Lineage):
     try:
         command = "cat intermediate/DOWN/*/PRESUMEout/all_SEQ_dead.out \
             > intermediate/all_dead.out 2> /dev/null; \
-            rm intermediate/DOWN/*/PRESUMEout/all_SEQ_dead.out"
+            rm intermediate/DOWN/*/PRESUMEout/all_SEQ_dead.out 2> /dev/null"
         subprocess.call(command, shell=True)
 
     except Exception as e:
