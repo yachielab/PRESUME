3784a81855e03913f729ebf156eb95df0719f8ab
UTNK <0ratnaramatn1n@gmail.com>
2020-10-04 18:30:33 +0900

    debug

--- nwk2fa.py
+++ nwk2fa.py
@@ -260,7 +260,7 @@ def shell_generator(shell_outfp, treefile_list, fastafile_list, indelfile_list, 
     for idx, (treefile, fastafile, indelfile) in enumerate(zip(treefile_list, fastafile_list, indelfile_list)):
         
         parent_name= treefile.split("/")[-1].split(".")[0]
-        OUTPUT_DIR = fasta_outfp+"/Down_{}".format(parent_name)
+        OUTPUT_DIR = fasta_outfp+"/{}".format(parent_name)
 
         with open("{}/downstream_{}.sh".format(shell_outfp, idx+1), 'w') as qf:
             qf.write("#!/bin/bash\n")
