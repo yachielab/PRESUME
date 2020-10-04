4e3a74f2d723a6eb5588b153fe523550a3b7b2ea
UTNK <0ratnaramatn1n@gmail.com>
2020-10-04 13:53:50 +0900

    debug

--- nwk2fa.py
+++ nwk2fa.py
@@ -254,7 +254,7 @@ def shell_generator(shell_outfp, treefile_list, fastafile_list, indelfile_list, 
     terminal_idx = 0
     for idx, (treefile, fastafile, indelfile) in enumerate(zip(treefile_list, fastafile_list, indelfile_list)):
         
-        parent_name= treefile.split("_")[1].split(".")[0]
+        parent_name= treefile.split("/")[-1].split("_")[1].split(".")[0]
         print (parent_name)
         OUTPUT_DIR = fasta_outfp+"/Down_{}".format(parent_name)
 
