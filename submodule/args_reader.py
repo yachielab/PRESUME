deb93ddf752eb431c5b0bbc4fdab8b6cbfaaf2fc
UTNK <0ratnaramatn1n@gmail.com>
2020-10-28 16:45:30 +0900

    debug

--- submodule/args_reader.py
+++ submodule/args_reader.py
@@ -94,7 +94,7 @@ class PARSED_ARGS():
         # on the doubling time of the SEQ
         if args.editprofile is not None:
             self.sub_prob_mtx_list = self.sub_mat_parser(args.editprofile)
-            args.editprofile       = os.getcwd() + "/" + args.editprofile.split("/")[-1]
+            args.editprofile       = os.getcwd() + "/" + args.editprofile
         elif(args.constant is not None):
             self.mu = [args.constant] * self.L
 
